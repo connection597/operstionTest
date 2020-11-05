09c573bfe8d68e062c31db05f5730bbc0cde5181
connection579 <wlswn4118@naver.com>
2020-11-05 17:11:28 +0900

    산술 연산자와 총점과 평균 구하기

---
+++ src/operation/OpersationEx1.java
@@ -0,0 +1,18 @@
+package operation;
+
+public class OpersationEx1 {
+
+	public static void main(String[] args) {
+		int mathScore = 90;
+		int engScore = 70;
+	    int korScore = 100;
+		
+		int totalScore =  mathScore + engScore +korScore ;
+		System.out.println(totalScore); // 총점 구하기
+		
+		double avgScore = totalScore/3.0;
+		System.out.println(avgScore); // 평균 구하기
+
+	}
+
+}
